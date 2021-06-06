2c5db41120762cbb37999d660231dc4f1e927792
Takahiro Konno <t.konno@shishimaro.co.jp>
2021-06-06 13:03:45 +0900

    :sparkles: HTMLテンプレートの動作確認用のテンプレートと経路追加

--- introduction_flask/app.py
+++ introduction_flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from typing import Dict
 
 app = Flask(__name__)
@@ -12,6 +12,11 @@ def hello_world() -> str:
 @app.route('/test')
 def test() -> Dict:
     return {'one': 1, 'two': 2}
+
+
+@app.route('/entry')
+def entry_page() -> str:
+    return render_template('entry.html')
 
 
 if __name__ == '__main__':
