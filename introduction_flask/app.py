86ac26ba40393abea9957ae226360f9bdd55e52a
Takahiro Konno <t.konno@shishimaro.co.jp>
2021-06-06 12:43:05 +0900

    :sparkles: テスト用に経路を追加

--- introduction_flask/app.py
+++ introduction_flask/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from typing import Dict
 
 app = Flask(__name__)
 
@@ -6,6 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world() -> str:
     return 'Hello World!'
+
+
+@app.route('/test')
+def test() -> Dict:
+    return {'one': 1, 'two': 2}
 
 
 if __name__ == '__main__':
